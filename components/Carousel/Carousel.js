950d38715f83ec6be7be7942ad04c6eea92ebdbf
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-11 14:43:24 -0400

    Attempted Carousel ... not working at this time

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,41 @@
 class Carousel {
-
+    constructor(img){
+        // assign this.img to the carousel image DOM reference
+        this.img = img;
+    }
 }
 
-let carousel = document.querySelector();
+// Reference to the carousel
+let carousel = document.querySelector(`.carousel`);
+console.log(carousel);
+
+// // Reference to the images
+// const carouselImages = document.querySelectorAll(`.carousel img`).forEach( img => new Carousel(img));
+
+// // Current Index
+// let carouselIndex = 0;
+
+// Left Button Reference
+// let leftButton = document.querySelector(`.left-button`);
+
+// // Left Button Click Event
+// leftButton.addEventListener('click', () => {
+//     console.log("left button clicked");
+// });
+
+// function moveBackwards(){
+//     console.log("left button clicked");
+// }
+
+// // Right Button Reference
+// const rightButton = document.querySelector(`.right-button`);
+
+// // Right Button Click Event
+// leftButton.addEventListener('click', () => moveForward);
+
+// function moveForward(){
+//     console.log("right button clicked");
+// }
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
@@ -12,13 +45,3 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
-
-// Reference to the carousel
-const carousel = document.querySelector(`.carousel`);
-
-// Reference to the images
-const carouselImages = document.querySelectorAll(`.carousel img`);
-
-// Current Index
-let carouselIndex = 0;
-
